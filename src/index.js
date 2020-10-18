970df6f07d461c7ef8007d90f3fbf62d7fd5001b
Evt-Nadya <49037722+Evt-Nadya@users.noreply.github.com>
2020-10-18 13:51:41 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var symbol = [];
+    var result = '';
+    for (i = 0; i < expr.length; i += 10) {
+        symbol.push(expr.slice(i, i + 10));
+    }
+
+    for (i = 0; i < symbol.length; i++) {
+        if (symbol[i] === "**********") {
+            result += " ";
+        } else {
+            var num = '';
+            for (j = 10; j > 0; j -= 2) {
+                if (symbol[i].slice(j - 2, j) == '10') {
+                    num = '.' + num
+                };
+                if (symbol[i].slice(j - 2, j) == '11') {
+                    num = '-' + num;
+                }
+            }
+            result += MORSE_TABLE[num];
+        }
+    }
+    return result;
 }
 
 module.exports = {
